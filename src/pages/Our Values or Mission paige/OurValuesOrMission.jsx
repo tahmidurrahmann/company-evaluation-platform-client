59c7efc86eb6103a0b61d0375b44269c222b23a6
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-01-17 00:32:46 +0600

    Our Values or Mission section added

---
+++ src/pages/Our Values or Mission paige/OurValuesOrMission.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurValuesOrMission = () => {
+    return (
+        <div>
+            <h1>Our values misson is rady</h1>
+        </div>
+    );
+};
+
+export default OurValuesOrMission;
