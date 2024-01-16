b63870dae5b7ff6cff3d567baf3134d82fc7ebd6
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-01-17 00:44:13 +0600

    p tag added

--- src/pages/Our Values or Mission paige/OurValuesOrMission.jsx
+++ src/pages/Our Values or Mission paige/OurValuesOrMission.jsx
@@ -4,6 +4,26 @@ const OurValuesOrMission = () => {
     return (
         <div>
             <h1>Our values misson is rady</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Doloribus quidem nostrum officia mollitia, est at minima
+                eveniet esse ratione cum amet, accusantium pariatur odit
+                maxime totam incidunt quaerat veritatis unde. Lorem ipsum
+                dolor sit, amet consectetur adipisicing elit. Accusamus,
+                autem aspernatur asperiores, repudiandae incidunt architecto
+                eligendi cumque quibusdam recusandae quam reiciendis doloremque
+                voluptatem aliquam fugiat iste, modi assumenda eveniet ab? Lorem
+                ipsum dolor sit, amet consectetur adipisicing elit. Dolore exercitationem
+                iusto soluta quasi maiores dolorum adipisci incidunt, assumenda praesentium
+                non eos quas doloremque, ullam error laborum impedit, at sapiente eum. Lorem,
+                ipsum dolor sit amet consectetur adipisicing elit. Doloribus eos commodi tempora
+                explicabo quis corrupti sapiente provident, incidunt doloremque et atque
+                cum odio, omnis ratione laboriosam facere, voluptate delectus optio? Lorem
+                ipsum dolor sit amet consectetur, adipisicing elit. Dolores eveniet deleniti
+                quam nulla, omnis ut! Autem assumenda molestiae voluptas, quo optio, maxime,
+                cupiditate pariatur ex labore sequi voluptatem velit non. Lorem ipsum dolor
+                sit amet consectetur adipisicing elit. Omnis, neque placeat. Sapiente fuga'
+                quisquam a voluptas deserunt id, quos neque laboriosam in, accusamus et aliquam
+                aliquid ex cumque laborum quaerat.</p>
         </div>
     );
 };
