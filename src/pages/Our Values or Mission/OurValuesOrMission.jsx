70fe4dd2aaaaf3c4783753b4b886a676199404d7
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-01-17 01:34:30 +0600

    p tag added

--- src/pages/Our Values or Mission/OurValuesOrMission.jsx
+++ src/pages/Our Values or Mission/OurValuesOrMission.jsx
@@ -3,6 +3,7 @@ const OurValuesOrMission = () => {
     return (
         <div>
             <h1>ourValues or Mission</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, facilis. Consectetur consequuntur asperiores dicta! Ullam maiores esse dolore. Laudantium dolor iusto sunt fugiat ipsam ullam maiores dolorem labore possimus ea?</p>
         </div>
     );
 };
