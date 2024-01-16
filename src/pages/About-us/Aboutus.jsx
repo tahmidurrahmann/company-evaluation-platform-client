5e6a0c74bcea048dd7e82d76995f0e1dbd798624
xunaiet <xunaiet28@gmail.com>
2024-01-17 00:47:35 +0600

    texting

--- src/pages/About-us/Aboutus.jsx
+++ src/pages/About-us/Aboutus.jsx
@@ -4,7 +4,7 @@ const Aboutus = () => {
     return (
         <div>
             <h1>hellow world</h1>
-            <h1>bye worlds world</h1>
+            <h1>bye worlds h  world</h1>
         </div>
     );
 };
