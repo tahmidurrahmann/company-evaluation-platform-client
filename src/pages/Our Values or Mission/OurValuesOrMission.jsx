037c796fca0b672f6701efd9c2f7682fe8e778b6
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-01-17 16:23:43 +0600

    Excellence section added

--- src/pages/Our Values or Mission/OurValuesOrMission.jsx
+++ src/pages/Our Values or Mission/OurValuesOrMission.jsx
@@ -1,9 +1,11 @@
+import Excellence from "./Excellence/Excellence";
 import Inclusivity from "./Inclusivity/Inclusivity";
 
 const OurValuesOrMission = () => {
     return (
         <div>
             <Inclusivity></Inclusivity>
+            <Excellence></Excellence>
         </div>
     );
 };
--- src/shared/NavBar/NavBar.jsx
+++ src/shared/NavBar/NavBar.jsx
@@ -30,7 +30,7 @@ const NavBar = () => {
                 isPending ? "pending" : isActive ? "font-semibold border-b-2 border-b-[#007cc7] transition text-lg text-white md:text-black" : "font-semibold hover:border-b-2 hover:border-b-[#007cc7] transition text-lg text-white md:text-black"
             }
         >
-            OurValuesOrMission
+            Missions
         </NavLink>
     </div>
 
