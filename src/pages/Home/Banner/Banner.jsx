2c206ca9a972943ac3192390bdf297afb8b94883
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2024-01-17 21:34:43 +0600

    added complete banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -6,11 +6,11 @@ const Banner = () => {
         <div className='relative'>
             <video className='w-full' autoPlay loop muted src={banner}></video>
             <div className='absolute top-0 bg-gradient-to-r from-[#15151500] to-[#151515] w-full h-full'>
-                <div className='absolute space-y-2 md:space-y-5 top-16 md:top-32 lg:top-16 xl:top-1/3 left-1/3 lg:left-3/4 xl:right-12 md:right-4 flex flex-col-reverse md:flex-col items-end lg:items-start'>
-                    <h1 className="text-white md:text-4xl text-center lg:text-left xl:text-5xl font-semibold">Boost Your IT Skills with Accurate Assessments</h1>
+                <div className='absolute space-y-2 md:space-y-3 lg:space-y-4 xl:space-y-5 top-[70px] md:top-[100px] lg:top-16 xl:top-1/3 left-1/3 lg:left-3/4 xl:right-20 right-4 flex flex-col items-end lg:items-start'>
+                    <h1 className="text-white md:text-4xl text-right lg:text-left xl:text-5xl font-semibold">Boost Your IT Skills with Accurate Assessments</h1>
                     <p className="text-white text-center lg:text-left  hidden md:flex">Empower your IT career with our all-in-one platform! Streamline assessments, track progress, and set meaningful goals. Elevate your skills with precision evaluations and receive continuous feedback. Designed for excellence and growth – your key to success in IT!</p>
-                    <button className="font-semibold bg-[#007cc7] px-2 md:px-3 py-1 rounded-md text-xs md:text-base text-white hover:scale-105 transition">Start Your IT Journey</button>
-                    <div className="flex items-center gap-2">
+                    <button className="font-semibold bg-[#007cc7] px-2 md:px-5 py-1 md:py-2 rounded-md text-xs md:text-base text-white hover:scale-105 transition">Start Your IT Journey</button>
+                    <div className="flex items-center gap-1">
                         <Rating
                             initialRating="4.6"
                             emptySymbol={<svg
@@ -19,7 +19,7 @@ const Banner = () => {
                                 viewBox="0 0 24 24"
                                 strokeWidth={1.5}
                                 stroke="currentColor"
-                                className="w-5 h-5 text-yellow-500"
+                                className="w-3 h-3 md:w-5 md:h-5 text-yellow-500"
                             >
                                 <path
                                     strokeLinecap="round"
@@ -31,7 +31,7 @@ const Banner = () => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 24 24"
                                 fill="currentColor"
-                                className="w-5 h-5 text-yellow-500"
+                                className="w-3 h-3 md:w-5 md:h-5 text-yellow-500"
                             >
                                 <path
                                     fillRule="evenodd"
