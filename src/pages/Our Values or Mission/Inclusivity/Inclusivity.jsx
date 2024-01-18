b7520d862dba204b0a1f0593c91b8bbebb0231d3
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-01-18 20:17:15 +0600

    responsive misson section for mobile , tablate and dextop

--- src/pages/Our Values or Mission/Inclusivity/Inclusivity.jsx
+++ src/pages/Our Values or Mission/Inclusivity/Inclusivity.jsx
@@ -3,17 +3,15 @@
 const Inclusivity = () => {
     return (
         <>
-            <div className="bg-[#55bcfc] h-[500px] grid grid-cols-2 gap-10">
+            <div className="bg-[#55bcfc] w-full h-[500px] gap-10 flex justify-center flex-col-reverse md:flex-row items-center">
                 <div className="flex justify-center items-center relative">
-                    <img className="ml-5  z-0" src="https://i.ibb.co/Htx5yDR/Untitled-design-1.jpg" alt="" />
+                    <img className="md:ml-5 md:w-full w-96" src="https://i.ibb.co/Htx5yDR/Untitled-design-1.jpg" alt="" />
                 </div>
 
-                <div className="flex justify-center items-center">
-                    <h1 className="font-bold text-7xl ">Inclusivity is Our Top Priority</h1>
+                <div className="mx-5">
+                    <h1 className="font-bold md:text-7xl text-5xl mt-10 ">Inclusivity is Our Top Priority</h1>
+                    <h1 className="lg:mt-40 md:mt-10 mt-5 text-xl">We believe in creating a platform that is accessible to everyone.</h1>
                 </div>
-            </div>
-            <div className="bg-gray-300 h-14 flex justify-center items-center text-lg">
-                <h1>We believe in creating a platform that is accessible to everyone.</h1>
             </div>
         </>
     );
