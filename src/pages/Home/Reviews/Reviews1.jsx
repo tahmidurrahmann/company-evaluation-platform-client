3121fa1fe32f4bf3fa3e6e5a890256a20ebcf44b
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2024-01-18 13:53:24 +0600

    added review section in home page

---
+++ src/pages/Home/Reviews/Reviews1.jsx
@@ -0,0 +1,36 @@
+import Marquee from "react-fast-marquee";
+import useReviews from '../../../hooks/useReviews';
+
+const Reviews1 = () => {
+
+    const [allReviews, isReviews] = useReviews();
+
+    if (isReviews) {
+        return <p>loading...</p>
+    }
+
+    return (
+        <div className="hidden md:flex my-4">
+                <Marquee>
+                    <div className="flex justify-center items-center gap-6">
+                        {
+                            allReviews.slice(0, 10)?.map(review => <div key={review?._id}>
+                                <div className="card w-96 h-48 bg-base-100 p-6">
+                                    <h1 className="font-bold pb-4 text-[#151746]">{review?.review}</h1>
+                                    <div className="flex items-center gap-4">
+                                        <img className="rounded-full w-[45px]" src={review?.userPicture} alt="" />
+                                        <div className="flex flex-col gap-1">
+                                            <h1 className="font-bold text-sm text-[#151746]">{review?.userName}</h1>
+                                            <h1 className="text-sm text-[#737490]">{review?.userEmail}</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>)
+                        }
+                    </div>
+                </Marquee>
+            </div>
+    );
+};
+
+export default Reviews1;
---
+++ src/pages/Home/Reviews/Reviews3.jsx
@@ -0,0 +1,51 @@
+import useReviews from "../../../hooks/useReviews";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+
+
+const Reviews3 = () => {
+
+    const [allReviews, isReviews] = useReviews();
+
+    if (isReviews) {
+        return <p>loading...</p>
+    }
+
+    const pagination = {
+        clickable: true
+    };
+
+    return (
+        <div className="flex md:hidden my-4 p-4">
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <div className="flex justify-center items-center">
+                    {
+                        allReviews?.map(review => <SwiperSlide key={review?._id}>
+                            <div >
+                                <div className="card bg-base-100 p-6">
+                                    <h1 className="font-bold pb-4 text-[#151746]">{review?.review}</h1>
+                                    <div className="flex items-center gap-4">
+                                        <img className="rounded-full w-[45px]" src={review?.userPicture} alt="" />
+                                        <div className="flex flex-col gap-1">
+                                            <h1 className="font-bold text-sm text-[#151746]">{review?.userName}</h1>
+                                            <h1 className="text-sm text-[#737490]">{review?.userEmail}</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>)
+                    }
+                </div>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Reviews3;
