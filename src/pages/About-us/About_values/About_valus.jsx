3d04065152ea1a9961e629e94a1314f844b2cda9
xunaiet <xunaiet28@gmail.com>
2024-01-18 19:04:41 +0600

    some edit about sestion

--- src/pages/About-us/About_values/About_valus.jsx
+++ src/pages/About-us/About_values/About_valus.jsx
@@ -7,8 +7,8 @@ const About_valus = () => {
     return (
         <div className="mb-20">
           <div className="">
-                <h1 className="font-bold text-2xl lg:ml-16  mb-2 uppercase">OUR VALUES</h1>
-                <h1 className="text-5xl lg:ml-16 mb-12 font-bold">Completely disintermediate <br /><span className="text-[#007cc7] ">excellent skills</span></h1>
+                <h1 className="font-bold text-2xl lg:ml-2  mb-2 uppercase">OUR VALUES</h1>
+                <h1 className="text-5xl lg:ml-2 mb-12 font-bold">Completely disintermediate <br /><span className="text-[#007cc7] ">excellent skills</span></h1>
           </div>
           <div className="grid mt-9 lg:grid-cols-3 gap-6 md:grid-cols-2 grid-cols-1 max-w-screen-xl mx-auto">
 
