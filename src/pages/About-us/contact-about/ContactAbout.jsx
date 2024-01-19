d62240d583c6bb8b49740fafe3338128d06b52fc
xunaiet <xunaiet28@gmail.com>
2024-01-19 11:11:49 +0600

    about us banner

--- src/pages/About-us/contact-about/ContactAbout.jsx
+++ src/pages/About-us/contact-about/ContactAbout.jsx
@@ -11,7 +11,7 @@ const ContactAbout = () => {
         <div className="hero-overlay bg-opacity-80"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Welcome </h1>
+            <h1 className="mb-3 text-5xl text-white font-bold">Welcome </h1>
             <p className="mb-5">Our platform is a catalyst for innovation, collaboration, and growth in the ever-evolving tech landscape.</p>
            <div className="flex gap-3 jus items-center ml-40  text-red-600">
               <Link to={'/'} className="hover:text-blue-500">Home  </Link>
