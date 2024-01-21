146dc79e17c9fed439b610b76d9563c2f4263ef4
Samrat Ahmmed <samratahammed29@gmail.com>
2024-01-21 23:19:17 +0600

    add a page

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import router from './router/router.jsx';
-import { HelmetProvider } from 'react-helmet-async';
-import {
-  QueryClient,
-  QueryClientProvider
-} from '@tanstack/react-query';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className='container mx-auto'>
-    <QueryClientProvider client={queryClient}>
-      <HelmetProvider>
-        <RouterProvider router={router} />
-      </HelmetProvider>
-    </QueryClientProvider>
+    <div className="container mx-auto">
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </QueryClientProvider>
     </div>
   </React.StrictMode>
-)
+);
