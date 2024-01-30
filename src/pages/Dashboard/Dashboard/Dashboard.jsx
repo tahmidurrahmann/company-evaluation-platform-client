aa619b1a568d296bc511914d2c321a4ce8416f05
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2024-01-31 03:09:14 +0600

    edited admin agreement request dashboard

--- src/pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard/Dashboard.jsx
@@ -130,7 +130,7 @@ function Dashboard(props) {
 
     return (
 
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: 'flex'}}>
             <CssBaseline />
             <AppBar
                 position="fixed"
