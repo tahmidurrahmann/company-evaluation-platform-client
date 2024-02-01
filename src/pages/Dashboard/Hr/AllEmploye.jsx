e0cfb2d3482528d62a709da2d55f28496f687ba7
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-02-01 16:01:15 +0600

    try to solve time essue

--- src/pages/Dashboard/Hr/AllEmploye.jsx
+++ src/pages/Dashboard/Hr/AllEmploye.jsx
@@ -71,7 +71,6 @@ const AllEmploye = () => {
                                             localStorage.setItem('data', JSON.stringify([...localItem, element]))
                                         }
                                     })
-
                                 }
                             }
                         })
