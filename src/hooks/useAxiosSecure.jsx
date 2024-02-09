91c53fa3d45d1002dae8772b4a0ac470cd2dad71
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2024-02-09 17:28:05 +0600

    added this

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 const useAxiosSecure = () => {
 
     const axiosSecure = axios.create({
-        baseURL: 'http://localhost:5000',
         // baseURL: 'http://localhost:5000',
+        baseURL: 'https://company-evaluation-platform-server.vercel.app',
     })
 
     return axiosSecure;
