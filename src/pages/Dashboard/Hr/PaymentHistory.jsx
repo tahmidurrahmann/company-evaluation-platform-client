846c8d8ba3dff8b5d7773f595d810c8f7409a5ca
xunaiet-faruk <xunaiet28@gmail.com>
2024-02-15 11:16:11 +0600

    imploy feedback

--- src/pages/Dashboard/Hr/PaymentHistory.jsx
+++ src/pages/Dashboard/Hr/PaymentHistory.jsx
@@ -1,64 +1,27 @@
-import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
-import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import usePayment from "../../../hooks/usePayment";
 import Loading from "../../../shared/Loading/Loading";
 
-
 const PaymentHistory = () => {
-    const {user} = useAuth()
-    const axiosSecure = useAxiosSecure()
+    const { user } = useAuth();
+    const [allPayments, isPayment] = usePayment();
 
-    const { data: payments=[],isPending } = useQuery({
-        queryKey: ["specificPayments", user?.email],
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/payments/${user?.email}`)
-            return res.data;
-        }
-      
-    })
-
-    if (isPending) {
-        <Loading />
+    if(isPayment){
+     return  <Loading/>
     }
+
+  
+    const userEmail = user ? user.email : "";
+
+    const filteredPayments = allPayments?.filter(item => item.employeeInfo.email === userEmail);
+
+   
+
     return (
         <div className="p-28">
-            <h1 className="text-4xl text-white font-bold text-center">Your Salary History</h1>
-           
+            <h1 className="text-4xl text-white font-bold text-center">Your Salary History {filteredPayments.length}</h1>
             <div>
-                <div className="overflow-x-auto mt-10">
-                    <table className="table table-zebra">
-                        {/* head */}
-                        <thead className="bg-white font-bold">
-                            <tr className="font-bold text-black">
-                                <th>Company</th>
-                                <th>Name</th>
-                                <th>Trangisition id</th>
-                                <th>Date</th>
-                                <th>Salary</th>
-                            </tr>
-                        </thead>
-                        <tbody className="text-white">
-
-                            {
-
-
-                                payments?.map(item => <tr key={item._id} className="text-white">
-                                    <th>{item?.name}</th>
-                                    <td>{item?.tranjectionId}</td>
-                                    <td>Quality Control Specialist</td>
-                                    <td>{item?.date}</td>
-                                    <td>Blue</td>
-                                </tr>)
-
-
-
-                            }
-
-
-
-                        </tbody>
-                    </table>
-                </div>
+                {/* Render your filtered payments here */}
             </div>
         </div>
     );
