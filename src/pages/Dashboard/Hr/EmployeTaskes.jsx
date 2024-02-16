0e1e2ff81ce47065815773e98d80912abe1a7fd3
xunaiet-faruk <xunaiet28@gmail.com>
2024-02-16 14:19:31 +0600

    payment history

--- src/pages/Dashboard/Hr/EmployeTaskes.jsx
+++ src/pages/Dashboard/Hr/EmployeTaskes.jsx
@@ -12,7 +12,6 @@ const EmployeTaskes = () => {
   const axiosPublic = useAxiosPublic();
   const [hrRequestCheck] = useHrRequestCheckedOrNot();
 
-
   const fetchTasks = useCallback(async () => {
     try {
       const res = await axiosPublic.get("/imployeeTasks");
@@ -24,6 +23,7 @@ const EmployeTaskes = () => {
     } catch (error) {
       console.log(error);
     }
+  }, [axiosPublic, hrRequestCheck]);
 
   useEffect(() => {
     axiosPublic
@@ -34,13 +34,17 @@ const EmployeTaskes = () => {
         );
         setTasks(taskFilter);
         setFilteredTasks(taskFilter);
-        console.log('task data is ',res.data); // Initialize filteredTasks with all tasks
+        console.log('task data is ', res.data); // Initialize filteredTasks with all tasks
       })
       .catch((error) => {
         console.log(error);
       });
 
   }, [axiosPublic, hrRequestCheck]);
+
+  useEffect(() => {
+    fetchTasks();
+  }, [fetchTasks]);
 
   const handleLike = (taskId) => {
     Swal.fire({
@@ -54,7 +58,7 @@ const EmployeTaskes = () => {
     }).then(async (result) => {
       if (result.isConfirmed) {
         try {
-          await axiosPublic.post(`/likeTask/${taskId}`);
+          await axiosPublic.post(`/likeTask/${ taskId }`);
           console.log("like");
           await fetchTasks(); // Refetch tasks after successfully liking the task
 
@@ -70,10 +74,6 @@ const EmployeTaskes = () => {
     });
   };
 
-  useEffect(() => {
-    fetchTasks();
-  }, [fetchTasks]);
-
   const handleCompletedStatus = () => {
     setFilteredTasks(tasks.filter((task) => task.status === "completed"));
   };
@@ -87,188 +87,184 @@ const EmployeTaskes = () => {
   };
 
 
-  return (
-    <div className="space-y-10 mt-5">
-      <div className="flex justify-center  items-center">
-        <div className="dropdown dropdown-end">
-          <button
-            tabIndex={0}
-            role="button"
-            className="flex items-center  gap-2 border px-2 font-semibold text-xl"
-          >
-            Filter <IoFilterSharp />
-          </button>
-          <ul
-            tabIndex={0}
-            className="dropdown-content z-[1] bg-black menu p-2 shadow border-2 border-blue-300  rounded-box w-52"
-          >
-            <li
-              onClick={handleCompletedStatus}
-              className="hover:bg-gray-500 rounded-xl"
+    return (
+      <div className="space-y-10 mt-5">
+        <div className="flex justify-center  items-center">
+          <div className="dropdown dropdown-end">
+            <button
+              tabIndex={0}
+              role="button"
+              className="flex items-center  gap-2 border px-2 font-semibold text-xl"
             >
-              <a>Completed</a>
-            </li>
-            <li
-              onClick={handleDoingStatus}
-              className="hover:bg-gray-500 rounded-xl"
+              Filter <IoFilterSharp />
+            </button>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] bg-black menu p-2 shadow border-2 border-blue-300  rounded-box w-52"
             >
-              <a>Doing</a>
-            </li>
-            <li
-              onClick={handleAllTask}
-              className="hover:bg-gray-500 rounded-xl"
-            >
-              <a>All</a>
-            </li>
-          </ul>
+              <li
+                onClick={handleCompletedStatus}
+                className="hover:bg-gray-500 rounded-xl"
+              >
+                <a>Completed</a>
+              </li>
+              <li
+                onClick={handleDoingStatus}
+                className="hover:bg-gray-500 rounded-xl"
+              >
+                <a>Doing</a>
+              </li>
+              <li
+                onClick={handleAllTask}
+                className="hover:bg-gray-500 rounded-xl"
+              >
+                <a>All</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
 
-      <div className="">
-        <div className="overflow-x-auto ml-24 mr-2">
-          <table className="table ">
-            <thead className="bg-gray-300 text-black font-bold">
-              <tr>
-                <th>Assignee</th>
-                <th>Task Name</th>
-                <th>Deadline</th>
-                <th>Audience</th>
-                {/* <th>Tags</th> */}
-                <th>Channel</th>
-                <th>Effort</th>
-                <th>Vote</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredTasks.map((element, index) => (
-                <tr
-                  className="h-24 border-b-2 text-white border-gray-300"
-                  key={index}
-                >
-                  <td className="flex justify-center mt-5  items-center gap-4">
-                    <div className="avatar -ml-10">
-                      <div className="w-8 rounded-full  border-2">
-                        <img src={element.employImage} alt="User Avatar" />
+        <div className="">
+          <div className="overflow-x-auto  mr-2 w-full">
+            <table className="table ">
+              <thead className="bg-gray-300 text-black font-bold">
+                <tr>
+                  <th>Assignee</th>
+                  <th>Task Name</th>
+                  <th>Deadline</th>
+                  <th>Audience</th>
+                  <th>Tags</th> 
+                  <th>Channel</th>
+                  <th>Effort</th>
+                  <th>Vote</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredTasks.map((element, index) => (
+                  <tr
+                    className="h-24 border-b-2 text-white border-gray-300"
+                    key={index}
+                  >
+                    <td className="flex justify-center mt-5  items-center gap-4">
+                      <div className="avatar -ml-10">
+                        <div className="w-8 rounded-full  border-2">
+                          <img src={element.employImage} alt="User Avatar" />
+                        </div>
                       </div>
-                    </div>
-                    {element.name}
-                  </td>
-                  <td> {element.addItem}</td>
-                  <td className="text-blue-500 font-bold">
-                    {element.timeAndLocal}
-                  </td>
-                  <td>
-                    <h1
-                      className={`${
-                        element.audience === "primium"
-                          ? "text-white font-bold"
-                          : element.audience === "busness"
-                          ? "text-white font-bold"
-                          : "font-bold text-white"
-                      }`}
-                    >
-                      {element.audience}
-                    </h1>
-                  </td>
+                      {element.name}
+                    </td>
+                    <td> {element.addItem}</td>
+                    <td className="text-blue-500 font-bold">
+                      {element.timeAndLocal}
+                    </td>
+                    <td>
+                      <h1
+                        className={`${element.audience === "primium"
+                            ? "text-white font-bold"
+                            : element.audience === "busness"
+                              ? "text-white font-bold"
+                              : "font-bold text-white"
+                          }`}
+                      >
+                        {element.audience}
+                      </h1>
+                    </td>
 
-                  <td>
-                    <h1
-                      className={`${element.tags === "lowProirity"
-                        ? "border-2 border-black rounded-full -ml-5 text-center text-white hover:text-white "
-                        : element.tags === "highPriority"
-                          ? "border-2 border-blue-300 -ml-5 rounded-full text-center text-white hover:text-white "
-                          : "border-2 border-blue-300 -ml-5 rounded-full text-center bg-gray-200 hover:text-white "
-                        }`}
-                    >
-                      {element.tags}
-                    </h1>
-                  </td>
+                    <td>
+                      <h1
+                        className={`${element.tags === "lowProirity"
+                          ? "border-2 border-black rounded-full -ml-5 text-center text-white hover:text-white "
+                          : element.tags === "highPriority"
+                            ? "border-2 -ml-5 rounded-full text-center border-blue-300 text-white hover:text-white "
+                            : "border-2  -ml-5 rounded-full text-center border-white  hover:text-white "
+                          }`}
+                      >
+                        {element.tags}
+                      </h1>
+                    </td>
 
-                  <td>
-                    <h1
-                      className={`${
-                        element.channel === "social"
-                          ? "font-bold "
-                          : element.channel === "blog"
-                          ? "font-bold"
-                          : element.channel === "press"
-                          ? "font-bold"
-                          : "font-bold"
-                      }`}
-                    >
-                      {element.channel}*
-                    </h1>
-                  </td>
-                  <td className="">
-                    <h1
-                      className={`${
-                        element.effort === "medium"
-                          ? "text-white font-bold"
-                          : element.tags === "low"
-                          ? "text-white font-bold"
-                          : element.effort === "high"
-                          ? "text-white font-bold"
-                          : "text-white font-bold"
-                      }`}
-                    >
-                      {element.effort}
-                    </h1>
-                  </td>
-                  <td className="">
-                    <h1
-                      className={`${
-                        element.effort === "medium"
-                          ? "text-white font-bold"
-                          : element.tags === "low"
-                          ? "text-white font-bold"
-                          : element.effort === "high"
-                          ? "text-white font-bold"
-                          : "text-white font-bold"
-                      }`}
-                    >
-                      {element.status === "completed" ? (
-                        <>
-                          {element.liked ? (
-                            <button
-                              disabled
-                              className="bg-blue-400 py-2 px-6 rounded-xl text-white font-semibold ml-1 border-2 shadow-xl shadow-blue-900 border-blue-400 hover:scale-105 transition"
-                            >
-                              <AiOutlineLike className="text-3xl" />
-                            </button>
-                          ) : (
+                    <td>
+                      <h1
+                        className={`${element.channel === "social"
+                            ? "font-bold "
+                            : element.channel === "blog"
+                              ? "font-bold"
+                              : element.channel === "press"
+                                ? "font-bold"
+                                : "font-bold"
+                          }`}
+                      >
+                        {element.channel}*
+                      </h1>
+                    </td>
+                    <td className="">
+                      <h1
+                        className={`${element.effort === "medium"
+                            ? "text-white font-bold"
+                            : element.tags === "low"
+                              ? "text-white font-bold"
+                              : element.effort === "high"
+                                ? "text-white font-bold"
+                                : "text-white font-bold"
+                          }`}
+                      >
+                        {element.effort}
+                      </h1>
+                    </td>
+                    <td className="">
+                      <h1
+                        className={`${element.effort === "medium"
+                            ? "text-white font-bold"
+                            : element.tags === "low"
+                              ? "text-white font-bold"
+                              : element.effort === "high"
+                                ? "text-white font-bold"
+                                : "text-white font-bold"
+                          }`}
+                      >
+                        {element.status === "completed" ? (
+                          <>
+                            {element.liked ? (
+                              <button
+                                disabled
+                                className=" py-2 px-6 rounded-xl text-white font-semibold ml-1 cursor-pointer  hover:scale-105 transition"
+                              >
+                                <AiOutlineLike className="text-3xl hover:text-blue-500" />
+                              </button>
+                            ) : (
+                              <button
+                                onClick={() => handleLike(element._id)}
+                                className=" py-2 px-6 rounded-xl text-white font-semibold ml-1  cursor-pointer hover:scale-105 transition"
+                              >
+                                  <AiOutlineDislike className="text-3xl hover:text-blue-500" />
+                              </button>
+                            )}
+                          </>
+                        ) : (
+                          <p className="flex flex-col items-center gap-1">
                             <button
                               onClick={() => handleLike(element._id)}
-                              className="bg-blue-400 py-2 px-6 rounded-xl text-white font-semibold ml-1 border-2 shadow-xl shadow-blue-900 border-blue-400 hover:scale-105 transition"
+                              disabled
+                              className=" py-2 px-6 rounded-xl text-white font-semibold ml-1"
                             >
-                              <AiOutlineDislike className="text-3xl" />
+                              <SiTodoist className="text-3xl mb-2" />
                             </button>
-                          )}
-                        </>
-                      ) : (
-                        <p className="flex flex-col items-center gap-1">
-                          <button
-                            onClick={() => handleLike(element._id)}
-                            disabled
-                            className="bg-blue-400 py-2 px-6 rounded-xl text-white font-semibold ml-1 border-2 shadow-xl shadow-blue-900 border-blue-400"
-                          >
-                            <SiTodoist className="text-3xl mb-2" />
-                          </button>
 
-                          <span className="badge badge-primary">
-                            {element.status}
-                          </span>
-                        </p>
-                      )}
-                    </h1>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                            <span className="badge badge-primary">
+                              {element.status}
+                            </span>
+                          </p>
+                        )}
+                      </h1>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default EmployeTaskes;
+  export default EmployeTaskes;
