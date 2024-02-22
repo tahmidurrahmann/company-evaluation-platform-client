071a4560c40669e390dba7f1c5203094c9fb0456
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2024-02-22 21:39:02 +0600

    added edited banner

--- src/pages/Home/Banner/Banner.tsx
+++ src/pages/Home/Banner/Banner.tsx
@@ -22,24 +22,21 @@ const Banner: React.FC = () => {
       <div className="absolute top-0 bg-gradient-to-r from-[#15151500] to-[#151515] w-full h-full">
         <div className="absolute top-0 bg-gradient-to-r from-[#15151500] to-[#151515] w-full h-full">
           <div className="absolute flex flex-col items-center justify-center space-y-4 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 lg:space-y-4 xl:space-y-10">
-            <h1
-              className="font-semibold text-center text-white md:text-4xl"
-            >
+            <h1 className="font-semibold text-center text-white md:text-4xl">
               Boost Your IT <span className="text-[#007cc7]">Skills</span> with
               Accurate <span className="text-[#007cc7]">Assessments</span>
             </h1>
-
-            <p className="text-sm text-center text-white">
-              Empower your IT career with our all-in-one platform! Streamline
-              assessments, track progress, and set meaningful goals. Elevate
-              your skills with precision evaluations and receive continuous
-              feedback. Designed for excellence and growth – your key to success
-              in IT!
-            </p>
+              <p className="text-sm text-center text-white hidden md:flex">
+                Empower your IT career with our all-in-one platform! Streamline
+                assessments, track progress, and set meaningful goals. Elevate
+                your skills with precision evaluations and receive continuous
+                feedback. Designed for excellence and growth – your key to success
+                in IT!
+              </p>
             <div className="flex items-center justify-center">
               <span
                 onClick={openModal}
-                className="font-semibold bg-[#007cc7] px-4 md:px-5 py-2 rounded-md text-xs md:text-base text-white hover:scale-105 transition flex justify-center items-center gap-4"
+                className="font-semibold bg-[#007cc7] px-2 md:px-5 py-1 md:py-2 rounded-md text-xs md:text-base text-white hover:scale-105 transition flex justify-center items-center gap-4"
               >
                 <GiJourney className="text-2xl" />
                 Start Your IT Journey
@@ -92,7 +89,7 @@ const Banner: React.FC = () => {
                             <div className="but-base"></div>
                           </button>
                         </Link>
-                                  
+
                       </div>
                       <div className="mt-4">
                         <span
