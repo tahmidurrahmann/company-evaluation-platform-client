c8b0e34eaeedc6c8654531c96652cdb12ff84a82
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-02-28 22:10:51 +0600

    daynamic monetary performance andaudiance lookalikes grap

--- src/pages/Reaching Chart/RachingBarMap.jsx
+++ src/pages/Reaching Chart/RachingBarMap.jsx
@@ -27,13 +27,6 @@ const RachingBarMap = () => {
             })
     }, [axiosPublic])
 
-    if (filterHr?.length > 0 && completedTaskpers?.length > 0 && Array.isArray(completedTaskpers)) {
-        filterHr.map(element => {
-            const taskFilter = completedTaskpers?.filter(elementTask => elementTask?.company === element?.company)
-            console.log(taskFilter);
-        })
-    }
-
     return (
         <div className="px-6 xl:px-0 max-w-screen-2xl mx-auto">
             <RaachingHome
