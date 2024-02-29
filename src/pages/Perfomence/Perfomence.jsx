cbb1fdc530fa73759daa2ea45baf59e2e47cbefe
xunaiet-faruk <xunaiet28@gmail.com>
2024-03-01 02:32:25 +0600

    responsive and some bug

--- src/pages/Perfomence/Perfomence.jsx
+++ src/pages/Perfomence/Perfomence.jsx
@@ -6,26 +6,28 @@ const Perfomence = () => {
     return (
         <>
             <div className="container mx-auto">
-                <div className="flex justify-between  pt-32">
+                <div className="flex flex-col lg:flex-row justify-between  pt-32">
                     <div className="">
-                        <h1 className="text-7xl p-5 font-bold ">Make Better People <br /> Decisions with <br />  Better Performance <br />  Data</h1>
+                        <h1 className="lg:text-7xl text-4xl md:text-5xl lg:text-start text-center  p-5 font-bold ">Make Better People <br /> Decisions with <br />  Better Performance <br />  Data</h1>
                         <p className="text-gray-600 p-5 ">One place to automatically track performance data and analyze results across time, teams, roles, or any other factor.</p>
-                        <button className="btn bg-blue-500 text-white border-b-2 border-b-white px-7 ml-3 ">Join Us</button>
+                    <div className="flex lg:justify-start justify-center">
+                            <button className="btn bg-blue-500  text-white border-b-2 border-b-white px-7 ml-3 ">Join Us</button>
 
+                        </div>
                     </div>
 
 
                     <div>
-                        <img className="w-[800px] ml-12" src="https://specie-admin-template.multipurposethemes.com/intro/images/image-1.png" alt="" />
+                        <img className="w-[800px] lg:ml-12 ml-0 md:pr-32 pr-12 mt-3 " src="https://specie-admin-template.multipurposethemes.com/intro/images/image-1.png" alt="" />
                     </div>
 
                 </div>
 
 
                 <div className="mt-12 p-12">
-                    <h1 className="text-center text-6xl font-bold">Stay Focused on <br /> Performance</h1>
+                    <h1 className="text-center text-4xl lg:text-6xl font-bold">Stay Focused on <br /> Performance</h1>
                     <div>
-                        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-20 ml-12">
+                        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-20 ml-0 lg:ml-12">
                             <div className="card w-96">
                                 <figure><LuBarChart4 className="text-9xl text-[#007cc7]" /></figure>
                                 <div className="card-body">
@@ -63,34 +65,35 @@ const Perfomence = () => {
 
 
 
-                <div className="flex justify-between items-center mt-20 mb-12">
+                <div className="flex lg:flex-row flex-col  justify-between  gap-8 items-center mt-20 mb-12">
+                  
                     <div className="mt-12">
                         <img className="w-[900px] relative" src="https://i.ibb.co/X7tQpcG/image.png" alt="" />
                         <div className="absolute -bottom-[1260px] left-[600px]">
                             <img className="w-[500px] rounded-2xl" src="https://i.ibb.co/NKYDWKZ/image.png" alt="" />
                         </div>
                     </div>
-
                     <div className="">
-                        <h1 className="text-5xl font-bold text-black">Connect Performance  &<br /> People Data</h1>
-                        <p className="text-gray-400">Bring together your performance data with employee data to answer<br /> the questions you care about most.</p>
+                        <h1 className="text-5xl font-bold text-black text-center lg:text-start">Connect Performance  &<br /> People Data</h1>
+                        <p className="text-gray-400 text-center lg:text-start">Bring together your performance data with employee data to answer<br /> the questions you care about most.</p>
                     </div>
+                    
                 </div>
 
 
 
-                <div className="flex justify-between items-center mt-80">
+                <div className="flex flex-col lg:flex-row justify-between items-center mt-80">
 
                     <div>
 
-                        <h1 className="text-6xl font-bold">The Best Support in Performance Management</h1>
-                        <p className="text-gray-500 mt-5">Account setup, live training sessions, a dedicated success manager, and phone support are always included. You <br /> will never be on your own with PerformYard.</p>
+                        <h1 className="text-6xl font-bold text-center lg:text-start">The Best Support in Performance Management</h1>
+                        <p className="text-gray-500 mt-5 text-center">Account setup, live training sessions, a dedicated success manager, and phone support are always included. You <br /> will never be on your own with PerformYard.</p>
 
                     </div>
 
 
                     <div>
-                        <img src="https://i.ibb.co/YLtFjTT/image.png" alt="" />
+                        <img src="https://i.ibb.co/YLtFjTT/image.png" className="p-5" alt="" />
                     </div>
                 </div>
 
@@ -99,7 +102,7 @@ const Perfomence = () => {
             </div>
 
             <div className="bg-gray-100 mt-60 h-[30vh] rounded-t-full p-4">
-                <h1 className="text-5xl text-center font-bold text-white pt-12">Streamline your performance management today</h1>
+                <h1 className="lg:text-5xl md:text-3xl  text-center font-bold text-white pt-12">Streamline your performance management today</h1>
                 <div className="flex justify-center space-y-4 mt-4">
                     <Link to={'/signin'}> <button className="cssbuttons-io-button transition mt-6">
                         Get started
